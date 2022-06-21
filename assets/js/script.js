219de0381a49c78d5c8c4d18adb09aae44d4da65
Royce Strange <roycest@swbell.net>
2022-06-21 00:29:39 -0500

    add questions array

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+var qustions = [
+    {
+        question: "Commonly used data types DO NOT inlcude:",
+        wrong: ["strings", "booleans", "numbers"],
+        correct: "alerts"
+    },
+    { 
+        question: "The condition in an if / else statement is enclosed with _____.",
+        wrong: ["quotes", "curly brackets", "square brackets"],
+        correct: "parenthesis"
+    },
+    { 
+        question: "Arrays in JavaScript can be used to store _____",
+        wrong: ["numbers and strings", "other arrays", "booleans"],
+        correct: "all of the above"
+    },
+    {
+        question: "String Values must be enclosed within _____ when being assigned to variables.",
+        wrong: ["commas", "curly brackets", "parenthesis"],
+        correct: "quotes"
+    },
+    {
+        question: "A very useful tool used during development and debugging for print content to the debugger is:",
+        wrong: ["JavaScript", "terminal/bash", "for loops"],
+        correct: "console.log"
+    }
+
+];
