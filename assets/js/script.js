71a17da2b85372ca8a76e41d54fdfc5a39b925c0
Royce Strange <roycest@swbell.net>
2022-06-23 17:53:18 -0500

    add a shuffling function to shuffle answers and questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -78,3 +78,15 @@ var loadHighScores = function () {
     // todo: return a sorted array based off score
     return JSON.parse(localStorage.getItem("highScores")) || [];
 };
+
+// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+var shuffleArray = function (array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+};
+
